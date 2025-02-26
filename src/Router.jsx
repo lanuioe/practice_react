4904a2880ff33a4d8308f641b5bf7b7d2109f6bb
junghyunlee0922 <self21c123@naver.com>
2025-02-26 19:32:33 +0900

    [📦Chore]: Router 변경 1. MainHeader -> MainPage로 변경.

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Login from "./lee/components/Login/Login";
-import MainHeader from "./lee/components/MainHeader/MainHeader";
+import MainPage from "./lee/pages/MainPage/MainPage";
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/Login/Page" element={<Login />}></Route>
-      <Route path="/Main/Page" element={<MainHeader />}></Route>
+      <Route path="/Main/Page" element={<MainPage />}></Route>
     </Routes>
   );
 };
